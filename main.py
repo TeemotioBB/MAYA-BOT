39acb240c83cfc40a1bf2322843f636b6cb44ad0
TeemotioBB <r66vtrhwkw@privaterelay.appleid.com>
2025-12-28 09:49:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ from telegram.ext import (
 )
 
 # ================= ENV =================
-TELEGRAM_TOKEN = os.getenv("8528168785:AAFlXEt1SGtyQDqYe4wt_f8MhN_JSKLYSj4")
-GROK_API_KEY = os.getenv("xai-WhzRhOWLna2aUD3A3Sv3siXwqVCTpIP9j5X1KNe1m8N7QB89Dzh20edMiTZbhB9tSaX4aMRKmCwsdpnD")
-PUSHINPAY_TOKEN = os.getenv("57758|Fd6yYTFbVw3meItiYnLjxnRN9W7i4jF467f4GfJj0fc9a3f5")
-WEBHOOK_SECRET = os.getenv("teste")
+TELEGRAM_TOKEN = ("8528168785:AAFlXEt1SGtyQDqYe4wt_f8MhN_JSKLYSj4")
+GROK_API_KEY = ("xai-WhzRhOWLna2aUD3A3Sv3siXwqVCTpIP9j5X1KNe1m8N7QB89Dzh20edMiTZbhB9tSaX4aMRKmCwsdpnD")
+PUSHINPAY_TOKEN = ("57758|Fd6yYTFbVw3meItiYnLjxnRN9W7i4jF467f4GfJj0fc9a3f5")
+WEBHOOK_SECRET = ("teste")
 PORT = int(os.getenv("PORT", 8080))
 
 # ================= CONFIG =================
@@ -192,5 +192,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
